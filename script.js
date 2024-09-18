264e98ba101f17de88f329d0596e6b676356735a
Arthur Affonso <arthuraffonsodejesus15@gmail.com>
2024-09-18 20:53:05 -0300

    Att 18/09

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
-var myIndex = 0;
+var myIndex1 = 0;
+var myIndex2 = 0;
+var myIndex3 = 0;
+
+carousel1();
+carousel2();
+carousel3();
+
+function carousel1() {
+  var i;
+  var x = document.getElementsByClassName("mySlides1");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex1++;
+  if (myIndex1 > x.length) {myIndex1 = 1}    
+  x[myIndex1-1].style.display = "block";  
+  setTimeout(carousel1, 4000); // Muda a imagem a cada 4 segundos
+}
+
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("mySlides2");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex2++;
+  if (myIndex2 > x.length) {myIndex2 = 1}    
+  x[myIndex2-1].style.display = "block";  
+  setTimeout(carousel2, 4000); // Muda a imagem a cada 4 segundos
+}
+
+function carousel3() {
+  var i;
+  var x = document.getElementsByClassName("mySlides3");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex3++;
+  if (myIndex3 > x.length) {myIndex3 = 1}    
+  x[myIndex3-1].style.display = "block";  
+  setTimeout(carousel3, 4000); // Muda a imagem a cada 4 segundos
+}
+
+/*var myIndex = 0;
 carousel();
 
 function carousel() {
@@ -11,4 +55,4 @@ function carousel() {
   if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].style.display = "block";  
   setTimeout(carousel, 4000); // Change image every 2 seconds
-}
+} */
